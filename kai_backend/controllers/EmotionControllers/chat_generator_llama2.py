1c424c81298407fe2a41d47474cf9dc5280e4d51
Fazlul-Karim-Shahed <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>
2024-04-03 02:24:35 +0600

    test

--- kai_backend/controllers/EmotionControllers/chat_generator_llama2.py
+++ kai_backend/controllers/EmotionControllers/chat_generator_llama2.py
@@ -10,17 +10,45 @@ async def chat_generator_llama():
         # tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-70b-hf")
         # model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-70b-hf")
 
-        model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
-        tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+        # model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+        # tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
 
-        prompt = request.get_json()['prompt']
-        inputs = tokenizer(prompt, return_tensors="pt")
+        # prompt = request.get_json()['prompt']
+        # inputs = tokenizer(prompt, return_tensors="pt")
 
-        # Generate
-        generate_ids = model.generate(inputs.input_ids, max_length=30)
-        print(tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0])
+        # # Generate
+        # generate_ids = model.generate(inputs.input_ids, max_length=30)
+        # print(tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0])
         
-        return jsonify({"message": tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0], "error": False })
+        # return jsonify({"message": tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0], "error": False })
+
+        from transformers import AutoTokenizer
+        import transformers
+        import torch
+
+        model = "meta-llama/Llama-2-7b-chat-hf"
+
+        tokenizer = AutoTokenizer.from_pretrained(model)
+        pipeline = transformers.pipeline(
+            "text-generation",
+            model=model,
+            torch_dtype=torch.float16,
+            device_map="auto",
+        )
+
+        sequences = pipeline(
+            'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n',
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=tokenizer.eos_token_id,
+            max_length=200,
+        )
+        for seq in sequences:
+            print(f"Result: {seq['generated_text']}")
+
+        return 'ok'
+
 
     except Exception as err:
         return jsonify({"message": str(err), "error": True })
